76c27c262d2d29930d7c6afe4341aa966bd51582
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-11-15 13:52:52 -0800

    make test for getting all the items in the inventory

--- __tests__/VendingMachine.spec.js
+++ __tests__/VendingMachine.spec.js
@@ -1,3 +1,13 @@
 // al the tests would go here
-const VendingMachine = require("../lib/vending-machine.js");
-let vendingMachine = new VendingMachine("../inventory.json");
+
+const inventory = require("../inventory.json");
+const VendingMachine = require("../lib/VendingMachine");
+const venderMachine = new VendingMachine(inventory);
+
+describe("getInventory:", () => {
+  describe("When get all the products of inventory", () => {
+    it("should return all products of inventory", () => {
+      expect(venderMachine.getAllInventoryProducts()).toEqual(inventory.items);
+    });
+  });
+});
