a809ac5b2f22bfad4f3104f317f85dd9fadb8e74
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-11-16 23:26:12 -0800

    Add Readme.md for the vending machine

---
+++ README.md
@@ -0,0 +1,41 @@
+# Vending Machine
+
+It demonstrates learnings in test-driven development through tests and functions for an imaginary vending machine.
+Vending machine and its tests, coded in one Javascript class with no interface, that basically has the following functionality:
+
+- Print inventory
+- Refill inventory
+- Re-supply change
+- Dispense inventory based on payment
+- Return change as coins (eg. \$0.25 is 1 quarter and 1 dime)
+
+## Technologies
+
+- JavaScript
+- Node
+- Jest
+
+## Screenshot
+
+## Installation
+
+Download the repo, then in its folder run:
+
+```
+npm install
+
+```
+
+## Run Command
+
+```
+npm run test
+
+```
+
+Optionally run one particular test suite using the name a file in the tests folder:
+
+```
+jest
+
+```
--- __tests__/VendingMachine.spec.js
+++ __tests__/VendingMachine.spec.js
@@ -1,86 +1,100 @@
-// al the tests would go here
-
 const inventory = require("../inventory.json");
 const VendingMachine = require("../lib/VendingMachine");
-const venderMachine = new VendingMachine(inventory);
 
-describe("getInventory:", () => {
-  describe("When get all the products of inventory", () => {
-    it("should return all products of inventory", () => {
-      expect(venderMachine.getAllInventoryProducts()).toEqual(inventory.items);
-    });
+let venderMachine;
+
+describe("Vending Machine", () => {
+  beforeEach(() => {
+    // Reset inventory
+    venderMachine = new VendingMachine(inventory);
   });
 
-  describe('When get inventory product by position="A1"', () => {
-    it("should return stock = 7", () => {
-      expect(venderMachine.getInventoryItemByPosition("A1")).toEqual(7);
-    });
-  });
-
-  describe("When get all the coins", () => {
-    it("should return all coins in the inventory", () => {
-      expect(venderMachine.getAllInventoryCoins()).toEqual(inventory.coins);
-    });
-  });
-
-  describe('When get the number of coins by coins="toonies"', () => {
-    it("should return numCoins = 50", () => {
-      expect(venderMachine.getInventoryCoinsByName("toonies")).toEqual(50);
-    });
-  });
-
-  describe('When get value of the coin by coins="quaters"', () => {
-    it("should return value = 0.25", () => {
-      expect(venderMachine.getCoinValueByName("quaters")).toEqual(0.25);
-    });
-  });
-
-  describe("setInventory:", () => {
-    describe('When set product inventory position= "A1",number = 2', () => {
-      it("should return stock = 9", () => {
-        expect(venderMachine.setProductStock("A1", 2)).toEqual(9);
+  describe("getInventory:", () => {
+    describe("When get all the products of inventory", () => {
+      it("should return all products of inventory", () => {
+        expect(venderMachine.getAllInventoryProducts()).toEqual(
+          inventory.items
+        );
       });
     });
-  });
 
-  describe('When set coins inventory name= "toonies",number = 25', () => {
-    it("should return coins=75", () => {
-      expect(venderMachine.setCoinsInventory("toonies", 25)).toEqual(75);
-    });
-  });
-
-  describe('When set products inventory position= "toonies",number = 26', () => {
-    it("should throw Error", () => {
-      expect(() => venderMachine.setCoinsInventory("toonies", 26)).toThrow(
-        "Over max spot available"
-      );
-    });
-  });
-  describe('When set new product to position= "A5", name=Jalepeno Durritos, price=3, stock=6, maxinventory=10', () => {
-    it("should return new product detail by position", () => {
-      expect(
-        venderMachine.setNewItemToPosition("A5", "Jalepeno Durritos", 3, 6, 10)
-      ).toEqual({
-        name: "Jalepeno Durritos",
-        price: 3,
-        stock: 6,
-        maxinventory: 10
+    describe('When get inventory product by position="A1"', () => {
+      it("should return stock = 7", () => {
+        expect(venderMachine.getInventoryItemByPosition("A1")).toEqual(7);
       });
     });
-  });
-  describe("Dispense Products:", () => {
-    describe("When giving change amount = 13.4", () => {
-      it("should return change = 13.4", () => {
-        expect(venderMachine.dispenseChanges(13.4)).toEqual(13.4);
+
+    describe("When get all the coins", () => {
+      it("should return all coins in the inventory", () => {
         expect(venderMachine.getAllInventoryCoins()).toEqual(inventory.coins);
       });
     });
-  });
-  describe('When getting amount = 5, position="A1"', () => {
-    it("should return true", () => {
-      expect(venderMachine.dispenseProduct("A1", 5)).toEqual(true);
-      expect(venderMachine.getAllInventoryCoins()).toEqual(inventory.coins);
-      expect(venderMachine.getInventoryItemByPosition("A1")).toEqual(8);
+
+    describe('When get the number of coins by coins="toonies"', () => {
+      it("should return numCoins = 50", () => {
+        expect(venderMachine.getInventoryCoinsByName("toonies")).toEqual(50);
+      });
+    });
+
+    describe('When get value of the coin by coins="quaters"', () => {
+      it("should return value = 0.25", () => {
+        expect(venderMachine.getCoinValueByName("quaters")).toEqual(0.25);
+      });
+    });
+
+    describe("setInventory:", () => {
+      describe('When set product inventory position= "A1",number = 2', () => {
+        it("should return stock = 9", () => {
+          expect(venderMachine.setProductStock("A1", 2)).toEqual(9);
+        });
+      });
+    });
+
+    describe('When set coins inventory name= "toonies",number = 25', () => {
+      it("should return coins=75", () => {
+        expect(venderMachine.setCoinsInventory("toonies", 25)).toEqual(75);
+      });
+    });
+
+    describe('When set products inventory position= "toonies",number = 26', () => {
+      it("should throw Error", () => {
+        expect(() => venderMachine.setCoinsInventory("toonies", 26)).toThrow(
+          "Over max spot available"
+        );
+      });
+    });
+    describe('When set new product to position= "A5", name=Jalepeno Durritos, price=3, stock=6, maxinventory=10', () => {
+      it("should return new product detail by position", () => {
+        expect(
+          venderMachine.setNewItemToPosition(
+            "A5",
+            "Jalepeno Durritos",
+            3,
+            6,
+            10
+          )
+        ).toEqual({
+          name: "Jalepeno Durritos",
+          price: 3,
+          stock: 6,
+          maxinventory: 10
+        });
+      });
+    });
+    describe("Dispense Products:", () => {
+      describe("When giving change amount = 13.4", () => {
+        it("should return change = 13.4", () => {
+          expect(venderMachine.dispenseChanges(13.4)).toEqual(13.4);
+          expect(venderMachine.getAllInventoryCoins()).toEqual(inventory.coins);
+        });
+      });
+    });
+    describe('When getting amount = 5, position="A1"', () => {
+      it("should return true", () => {
+        expect(venderMachine.dispenseProduct("A1", 5)).toEqual(true);
+        expect(venderMachine.getAllInventoryCoins()).toEqual(inventory.coins);
+        expect(venderMachine.getInventoryItemByPosition("A1")).toEqual(8);
+      });
     });
   });
 });
