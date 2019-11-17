d234dd810f50bc109b9a664ef809952b09704444
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-11-16 18:50:03 -0800

    make test for setting inventory with stock and coins

--- __tests__/VendingMachine.spec.js
+++ __tests__/VendingMachine.spec.js
@@ -22,4 +22,30 @@ describe("getInventory:", () => {
       expect(venderMachine.getAllInventoryCoins()).toEqual(inventory.coins);
     });
   });
+
+  describe('When get the number of coins by coins="toonies"', () => {
+    it("should return numCoins = 50", () => {
+      expect(venderMachine.getInventoryCoinsByName("toonies")).toEqual(50);
+    });
+  });
+
+  describe('When get value of the coin by coins="quaters"', () => {
+    it("should return value = 0.25", () => {
+      expect(venderMachine.getCoinValueByName("quaters")).toEqual(0.25);
+    });
+  });
+
+  describe("setInventory:", () => {
+    describe('When set product inventory position= "A1",number = 2', () => {
+      it("should return stock = 9", () => {
+        expect(venderMachine.setProductStock("A1", 2)).toEqual(9);
+      });
+    });
+  });
+
+  describe('When set coins inventory name= "toonies",number = 25', () => {
+    it("should return coins=75", () => {
+      expect(venderMachine.setCoinsInventory("toonies", 25)).toEqual(75);
+    });
+  });
 });
